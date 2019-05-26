5fc31cbd7fc7a4bd5fb5fd44b300ff6fec5162ae
ratiotile <eujain@gmail.com>
2019-05-26 11:58:44 -0400

    edited karun script to enable --static .html file generation

--- karax/tools/karun.nim
+++ karax/tools/karun.nim
@@ -12,8 +12,7 @@ import os,
 
 const
   css = """
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.4/css/bulma.min.css">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="../src/style.css">
 """
   html = """
 <!DOCTYPE html>
@@ -23,7 +22,9 @@ const
   $2
 </head>
 <body id="body">
-<div id="ROOT" />
+<div id="ROOT">
+  $3
+</div>
 <script type="text/javascript" src="$1.js"></script>
 </body>
 </html>
@@ -38,14 +39,24 @@ proc build(name: string, rest: string, selectedCss: string, run: bool) =
   createDir("nimcache")
   exec("nim js --out:nimcache/" & name & ".js " & rest)
   let dest = "nimcache" / name & ".html"
-  writeFile(dest, html % [name, selectedCss])
+  writeFile(dest, html % [name, selectedCss, ""])
   if run: openDefaultBrowser(dest)
+
+proc buildStatic(name: string, rest: string, selectedCss: string, run: bool) =
+  echo("Build static page...")
+  # first compile site as executable
+  createDir("nimcache")
+  exec("nim c --out:nimcache/" & name & ".exe " & rest)
+  exec("nimcache\\" & name & ".exe")
+  exec("nim js --out:nimcache/" & name & ".js " & rest)
+  if run: openDefaultBrowser("http://localhost:8000/" / name & ".html") # need to ensure this matches staticsite
 
 proc main =
   var op = initOptParser()
   var rest = op.cmdLineRest
   var file = ""
   var run = false
+  var genStatic = false
   var selectedCss = ""
   var watch = false
   var files: Table[string, Time] = {"path": getLastModificationTime(".")}.toTable
@@ -61,6 +72,9 @@ proc main =
       of "css":
         selectedCss = css
         rest = rest.replace("--css ")
+      of "static":
+        genStatic = true
+        rest = rest.replace("--static ")
       else: discard
     of cmdShortOption:
       if op.key == "r":
@@ -74,7 +88,10 @@ proc main =
 
   if file.len == 0: quit "filename expected"
   let name = file.splitFile.name
-  build(name, rest, selectedCss, run)
+  if genStatic:
+    buildStatic(name, rest, selectedCss, run)
+  else:
+    build(name, rest, selectedCss, run)
   echo("after build")
   if watch:
     # TODO: launch http server
